524347c7306fd350e51990a9e2cf6775759fb1c8
DistantDig <jameswaller711@gmail.com>
2024-04-20 11:24:39 -0700

    implemented new EzLib datagen methods

--- src/main/java/net/birb_feather/other_mod/OtherModDataGenerator.java
+++ src/main/java/net/birb_feather/other_mod/OtherModDataGenerator.java
@@ -1,11 +1,19 @@
 package net.birb_feather.other_mod;
 
+import net.distantdig.ezLib.EzLibDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.core.RegistrySetBuilder;
 
 public class OtherModDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
-	}
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        EzLibDataGenerator.runDataGen(fabricDataGenerator);
+    }
+
+    @Override
+    public void buildRegistry(RegistrySetBuilder registryBuilder) {
+        EzLibDataGenerator.runBuildRegistry(registryBuilder);
+    }
 }
